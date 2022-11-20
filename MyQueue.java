f92b4a36aaddb1116906006fbbc8d29a2e15c332
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-21 00:37:44 +0530

    MyQueue class UC3

--- MyQueue.java
+++ MyQueue.java
@@ -1,1 +1,15 @@
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
 
+    public MyQueue(){
+        myLinkedList = new MyLinkedList();
+    }
+    public void enque(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printMyQueue() {
+        myLinkedList.printMyNodes();
+    }
+
+}
