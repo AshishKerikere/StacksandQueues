e2ade9ae6514e939d1e1f719f8e9843866fc7b45
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-21 00:32:22 +0530

    MyStackTest class UC1

--- MyStackTest.java
+++ MyStackTest.java
@@ -1,19 +1,14 @@
-public class MyStack {
-    private final MyLinkedList myLinkedList;
-
-    public MyStack(){
-        this.myLinkedList = new MyLinkedList();
-    }
-    public void push(INode myNode) {
-        myLinkedList.add(myNode);
-
-    }
-
-    public void printStack() {
-        myLinkedList.printMyNodes();
-    }
-
-    public INode peak() {
-        return myLinkedList.head;
+public class MyStackTest {
+    public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        myStack.printStack();
+        INode pop = myStack.pop();
+        System.out.println(pop.getKey());
     }
 }
