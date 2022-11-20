a4683a654ab76d5a89ab499de43c914cf7ab6594
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-21 00:38:42 +0530

    MyQueueTest class UC3

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -1,1 +1,12 @@
-
+public class MyQueueTest {
+    public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enque(myFirstNode);
+        myQueue.enque(mySecondNode);
+        myQueue.enque(myThirdNode);
+        myQueue.printMyQueue();
+    }
+}
